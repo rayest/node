46d84cbe96dbaf1b898457c33d2054d8d06f639e
lirui <634178463@qq.com>
2017-04-03 19:10:33 +0800

    express module POST request

--- public/javascripts/express/express.js
+++ public/javascripts/express/express.js
@@ -5,8 +5,14 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (request, response) {
-    response.send('Hello World');
+app.get('/get', function (request, response) {
+    console.log('发送了GET请求, 用于获取资源');
+    response.send('GET is used to get some resource');
+});
+
+app.post('/create', function (request, response) {
+    console.log('发送了POST请求，用于创建资源');
+    response.send("POST is used to create a new resource");
 });
 
 var server = app.listen(8080, function () {
